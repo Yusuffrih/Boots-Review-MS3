abf80c04c057053a8349e9ee7cadac8fdea2e55a
Yusuffrih <yusuffrih@hotmail.com>
2021-09-03 11:23:41 +0000

    Created basic view for manage categories

--- app.py
+++ app.py
@@ -252,6 +252,14 @@ def delete_review(review_id):
         return redirect(url_for("reviews"))
 
 
+@app.route("/manage_categories")
+def manage_categories():
+    categories = list(mongo.db.categories.find().sort("categories_name", 1))
+    makes = list(mongo.db.categories.find().sort("name", 1))
+    return render_template("pages/categories.html", categories=categories,
+                           makes=makes)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
