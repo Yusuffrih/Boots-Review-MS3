4578997209d68493d17b47e8a6bc48be63ba567c
Yusuffrih <yusuffrih@hotmail.com>
2021-09-12 20:15:13 +0000

    Removed bootstrap form validation js

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,7 @@
 const chevronRef = document.querySelectorAll("#chevron")
-    
+const needsValidationFormRef = document.querySelectorAll('needs-validation');
+
 chevronRef.addEventListener("click", e => {
-chevronRef.classList.remove('fa-chevron-down');
-chevronRef.classList.add('fa-chevron-up');
+  chevronRef.classList.remove('fa-chevron-down');
+  chevronRef.classList.add('fa-chevron-up');
 });
-
-
-// Bootstrap form validation code from documentation
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
