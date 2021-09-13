cbdb1fa40c2726153b2e0c1a40563ec76bbf7657
Yusuffrih <yusuffrih@hotmail.com>
2021-09-13 20:56:26 +0000

    Deleted unsused code

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-const chevronRef = document.querySelectorAll("#chevron")
-const needsValidationFormRef = document.querySelectorAll('needs-validation');
-
-chevronRef.addEventListener("click", e => {
-  chevronRef.classList.remove('fa-chevron-down');
-  chevronRef.classList.add('fa-chevron-up');
-});
